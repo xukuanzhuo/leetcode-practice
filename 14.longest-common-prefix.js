8415cb2942cf9eb40d346693ad427b9cae64afd8
xkz <xukuanzhuo@xukuanzuodeMBP2.lan>
2019-03-17 23:44:10 +0800

    longest-common-prefix

---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (32.95%)
+ * Total Accepted:    421.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * Write a function to find the longest common prefix string amongst an array
+ * of strings.
+ * 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * 
+ * 
+ * Note:
+ * 
+ * All given inputs are in lowercase letters a-z.
+ * 
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs.length) return '';
+  
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let str of strs) {
+      if (str[i] !== strs[0][i]) {
+        return str.slice(0, i);
+      }
+    }
+  }
+  
+  return strs[0];
+};
+
